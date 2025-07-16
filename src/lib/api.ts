3497319d378b4faade6d57e67af9799e080272bd
marketlubes@gmail.com <marketlubedevelopers@gmail.com>
2025-07-16 09:58:48 +0530

    fix test

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -90,7 +90,6 @@ export const contactApi = {
     formData.append("city", data.city);
     formData.append("zip", data.zip);
 
-
     if (data.image) {
       formData.append("image", data.image);
     }
