d9cd17a2dcffabacee0f2616e1ab3464f5d3df77
marketlubeDev <marketlubedevelopers@gmail.com>
2025-09-27 18:14:20 +0530

    cors

--- skillhands-server/server.js
+++ skillhands-server/server.js
@@ -37,7 +37,7 @@ app.use(
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    // allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
